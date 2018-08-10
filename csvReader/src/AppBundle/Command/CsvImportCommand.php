64581394f5ae47ad756d970e02864984cbce519d
jesus yepes <jesusyepes.1205@gmail.com>
2018-08-10 21:33:55 +0200

    Arreglando setter en shipCity y shipState / funcion que compara si es nulo o no las cadena leida (c/columna) del archivo

--- csvReader/src/AppBundle/Command/CsvImportCommand.php
+++ csvReader/src/AppBundle/Command/CsvImportCommand.php
@@ -68,42 +68,42 @@ class CsvImportCommand extends Command
         foreach ($results as $row) {
 
             $postalDua = (new Postal())
-                ->setTrackingNumber($row["TrackingNumber"])
-                ->setConocimientoAereo($row["ConocimientoAereo"])
-                ->setReference($row["Reference"])
-                ->setBagLabel($row["BagLabel"])
-                ->setOrigin($row["Origin"])
-                ->setDestination($row["Destination"])
-                ->setSumaria($row["Sumaria"])
+                ->setTrackingNumber($this->validarCadenaVacia($row["TrackingNumber"]))
+                ->setConocimientoAereo($this->validarCadenaVacia($row["ConocimientoAereo"]))
+                ->setReference($this->validarCadenaVacia($row["Reference"]))
+                ->setBagLabel($this->validarCadenaVacia($row["BagLabel"]))
+                ->setOrigin($this->validarCadenaVacia($row["Origin"]))
+                ->setDestination($this->validarCadenaVacia($row["Destination"]))
+                ->setSumaria($this->validarCadenaVacia($row["Sumaria"]))
                 ->setPartida($row["Partida"])
-                ->setInternalAccountNumber($row["InternalAccountNumber"])
-                ->setShipperName($row["ShipperName"])
-                ->setShipAdd1($row["ShipAdd1"])
-                ->setShipAdd2($row["ShipAdd2"])
-                ->setShipAdd3($row["ShipAdd3"])
-                ->setCity($row["City"])
-                ->setState($row["State"])
+                ->setInternalAccountNumber($this->validarCadenaVacia( $row["InternalAccountNumber"]))
+                ->setShipperName($this->validarCadenaVacia( $row["ShipperName"]))
+                ->setShipAdd1($this->validarCadenaVacia($row["ShipAdd1"]))
+                ->setShipAdd2($this->validarCadenaVacia($row["ShipAdd2"]))
+                ->setShipAdd3($this->validarCadenaVacia($row["ShipAdd3"]))
+                ->setShipCity($this->validarCadenaVacia($row["ShipCity"]))
+                ->setShipState($this->validarCadenaVacia($row["ShipState"]))
                 ->setShipZip($row["ShipZip"])
-                ->setShipCountryCode($row["ShipContryCode"])
-                ->setNif($row["NIF"])
-                ->setConsignee($row["Consignee"])
-                ->setAddress1($row["Address1"])
-                ->setAddress2($row["Address2"])
-                ->setAddress3($row["Address3"])
-                ->setCity($row["City"])
-                ->setState($row["State"])
+                ->setShipCountryCode($this->validarCadenaVacia($row["ShipContryCode"]))
+                ->setNif($this->validarCadenaVacia($row["NIF"]))
+                ->setConsignee($this->validarCadenaVacia($row["Consignee"]))
+                ->setAddress1($this->validarCadenaVacia($row["Address1"]))
+                ->setAddress2($this->validarCadenaVacia($row["Address2"]))
+                ->setAddress3($this->validarCadenaVacia($row["Address3"]))
+                ->setCity($this->validarCadenaVacia($row["City"]))
+                ->setState($this->validarCadenaVacia($row["State"]))
                 ->setZip($row["Zip"])
-                ->setCountryCode($row["CountryCode"])
-                ->setEmail($row["Email"])
-                ->setPhone($row["Phone"])
+                ->setCountryCode($this->validarCadenaVacia($row["CountryCode"]))
+                ->setEmail($this->validarCadenaVacia($row["Email"]))
+                ->setPhone($this->validarCadenaVacia($row["Phone"]))
                 ->setPieces($row["Pieces"])
                 ->setTotalWeight($row["TotalWeight"])
-                ->setWeightUOM($row["WeightUOM"])
-                ->setTotalValue($row["TotalValue"])
-                ->setCurrency($row["Currency"])
-                ->setIncoterms($row["Incoterms"])
-                ->setService($row["Service"])
-                ->setItemDescription($row["ItemDescription"])
+                ->setWeightUOM($this->validarCadenaVacia($row["WeightUOM"]))
+                ->setTotalValue($this->validarCadenaVacia($row["TotalValue"]))
+                ->setCurrency($this->validarCadenaVacia($row["Currency"]))
+                ->setIncoterms($this->validarCadenaVacia($row["Incoterms"]))
+                ->setService($this->validarCadenaVacia($row["Service"]))
+                ->setItemDescription($this->validarCadenaVacia($row["ItemDescription"]))
                 ->setItemHsCode($row["ItemHSCode"])
                 ->setItemQuantity($row["ItemQuantity"])
                 ->setItemValue($row["ItemValue"])
@@ -118,5 +118,20 @@ class CsvImportCommand extends Command
 
         $io->progressFinish();
         $io->success('Command exited cleanly!');
+    }
+
+
+    /**
+     * valida si el valor dentro del documentp es vacio, escribe null
+     * en la base de datos
+     * @param $valor
+     * @return string
+     */
+    public function validarCadenaVacia($valor){
+        if(trim($valor) == ''){
+            return 'NULL';
+        }else{
+            return $valor;
+        }
     }
 }
