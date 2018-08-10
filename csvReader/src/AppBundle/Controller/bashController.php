16acbc7d06b116d55922b833abf74b9e90036c17
jesus yepes <jesusyepes.1205@gmail.com>
2018-08-10 23:45:36 +0200

    creacion del controlador prueba, para mostrar solo un echo

---
+++ csvReader/src/AppBundle/Controller/bashController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maggie
+ * Date: 10/08/18
+ * Time: 21:39
+ */
+
+namespace AppBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class bashController
+ * @package AppBundle\Controller
+ */
+class bashController extends Controller
+{
+    /**
+     * @return Response
+     * @Route("Lucky/number")
+     * @throws \Exception
+     */
+    public function numberAction()
+    {
+        $number = random_int(0,100);
+       // system(..\);
+        system( './../bash/prueba.sh')."\n";
+        //print exec("echo hola")."\n";
+        return new Response('<html><body>Lucky number is :'.$number. '</body>');
+    }
+
+
+}
