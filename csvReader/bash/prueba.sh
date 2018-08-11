9426da59735584bcef81073249c30a54a0557b57
jesus yepes <jesusyepes.1205@gmail.com>
2018-08-11 21:41:55 +0200

    preparando archivo para el sbatch

--- csvReader/bash/prueba.sh
+++ csvReader/bash/prueba.sh
@@ -1,12 +1,26 @@
-#!/bin/bash
+
+#!/bin/bash/env php
 
 echo "hola mundo"
 #php bin/console csv:import
+php ./../bin/console csv:import
 today=`date +%Y%m%d`
 echo $today
 #while true; do
+#php ./../bin/console csv:import
 
-
-echo "hola"
+#echo "hola"
 #sleep 0.05
 #done
+
+##!/usr/bin/env php
+## app/console
+#<?php
+
+#use AppBundle\Command\CsvImportCommand;
+#use Symfony\Component\Console\Application;
+
+#$application = new Application();
+#$application->add(new CsvImportCommand);
+#$application->run();
+#>
