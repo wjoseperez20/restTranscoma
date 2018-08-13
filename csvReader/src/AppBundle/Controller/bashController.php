a04d2fdec04f3f9093fb5c8dc00fd46793a6abc5
jesus yepes <jesusyepes.1205@gmail.com>
2018-08-13 17:23:51 +0200

    fixing and adding loggers into bashController class

--- csvReader/src/AppBundle/Controller/bashController.php
+++ csvReader/src/AppBundle/Controller/bashController.php
@@ -7,10 +7,13 @@
  */
 
 namespace AppBundle\Controller;
+
+use Monolog\Logger;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Monolog\Handler\StreamHandler;
 
 /**
  * Class bashController
@@ -19,7 +22,6 @@ use Symfony\Component\HttpFoundation\Request;
 class bashController extends Controller
 {
 
-
     /**
      * @return Response
      * @Route("Lucky/number")
@@ -27,27 +29,27 @@ class bashController extends Controller
      */
     public function numberAction()
     {
-        $number = random_int(0,100);
 
-       // system(..\);
-      //  $obj = new bashController();
+        $number = random_int(0, 100);
 
-        system( './../csvReaderDaemon.sh')."\n";
-        $logger =$this->get('logger');
-        $logger->info('I just got the logger -----');
-        $logger->err('An error occurredffff');
-        $logger->alert('An error occurredffff');
-        $logger->emergency('An error occurredffff');
-        $logger->warning('An error occurredffff');
-        $logger->critical('I left the oven on!', array(
-            // include extra "context" info in your logs
-            'cause' => 'in_hurry',
-        ));
+        // system(..\);
+        //  $obj = new bashController();
+        system('./../csvReaderDaemon.sh') . "\n";
+        $logger = $this->get('logger');
+        $logger->info('This process was started in '.bashController::class);
 
+//        $logger->err('An error occurredffff');
+//        $logger->alert('An error occurredffff');
+//        $logger->emergency('An error occurredffff');
+//        $logger->warning('An error occurredffff');
+//        $logger->critical('I left the oven on!', array(
+//            // include extra "context" info in your logs
+//            'cause' => 'in_hurry',
+//        ));
 
 
         //print exec("echo hola")."\n";
-        return new Response('<html><body>Lucky number is :'.$number. '</body>');
+        return new Response('<html><body>Lucky number is :' . $number . '</body>');
     }
 
 
