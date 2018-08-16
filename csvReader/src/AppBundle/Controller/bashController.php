fa2024134633b4b48dbb7376e12d5df8bc591625
jesus yepes <jesusyepes.1205@gmail.com>
2018-08-16 22:23:31 +0200

    Adaptando la fabrica Logger en bashControlle

--- csvReader/src/AppBundle/Controller/bashController.php
+++ csvReader/src/AppBundle/Controller/bashController.php
@@ -8,6 +8,7 @@
 
 namespace AppBundle\Controller;
 
+use Factory\LoggerFactory;
 use Monolog\Logger;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,6 +23,12 @@ use Psr\Log\LoggerInterface;
  */
 class bashController extends Controller
 {
+    /**
+     * Constantes para esteblecer parametros de los loggers
+     */
+    const CLASS_NAME = bashController::class;
+    const LOG_DIRECTORY ='../var/logs/Controller/dev.log';
+
 
     /**
      * @return Response
@@ -32,11 +39,20 @@ class bashController extends Controller
     {
 
         $number = random_int(0, 100);
+        $logger = LoggerFactory::getLogger(self::CLASS_NAME);
+        $handler = LoggerFactory::getStreamHandler(self::LOG_DIRECTORY);
+        $logger->pushHandler($handler);
 
         system('./../csvReaderDaemon.sh') . "\n";
 //        $logger = $this->get('logger');
-//        $logger->info('This process was started in '.bashController::class .'into in :' .bashController::numberAction());
-        $this->get('app.listener.command_exception')->exampleMethod('kakkakaka');
+        $logger->info('This process was started in .bashController::class into in : .bashController::numberAction()');
+
+        /**
+         * para llamar al servicio de monolog ubicado en services.yml y config_dev
+         * $this->get('app.listener.command_exception')->exampleMethod('kakkakaka');
+         *
+         */
+
         //print exec("echo hola")."\n";
         return new Response('<html><body>Lucky number is :' . $number . '</body>');
 
