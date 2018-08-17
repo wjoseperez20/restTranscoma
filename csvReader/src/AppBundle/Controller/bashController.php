ddb59eeeb3aa8af91115e72d966549cbd15a1b1a
jesus yepes <jesusyepes.1205@gmail.com>
2018-08-17 23:52:25 +0200

    Agregando bloque de excepciones y activacion del .sh

--- csvReader/src/AppBundle/Controller/bashController.php
+++ csvReader/src/AppBundle/Controller/bashController.php
@@ -10,6 +10,7 @@ namespace AppBundle\Controller;
 
 use Factory\LoggerFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -33,19 +34,34 @@ class bashController extends Controller
 	 */
 	public function numberAction()
 	{
+		try
+		{
+			$number = random_int(0, 100);
+			$logger = LoggerFactory::getLogger(self::CLASS_NAME);
+			$handler = LoggerFactory::getStreamHandler(self::LOG_DIRECTORY);
+			$logger->pushHandler($handler);
 
-		$number = random_int(0, 100);
-		$logger = LoggerFactory::getLogger(self::CLASS_NAME);
-		$handler = LoggerFactory::getStreamHandler(self::LOG_DIRECTORY);
-		$logger->pushHandler($handler);
+			system('./../csvReaderDaemon.sh') . "\n";
 
-        // system('./../csvReaderDaemon.sh') . "\n";
+			$logger->info('This process was started in .bashController::class into in : .bashController::numberAction()');
 
-		$logger->info('This process was started in .bashController::class into in : .bashController::numberAction()');
+			//print exec("echo hola")."\n";
+			return new Response('<html><body>Lucky number is :' . $number .	' .</body>');
+		}
+		catch (RuntimeException $ee)
+		{
+			return new Response(" Tiempo de ejecucion excedido ". $ee, Response::HTTP_REQUEST_TIMEOUT);
+			$logger->error("({$ee->getCode()}) Message: '{$ee->getMessage()}' in file: '{$ee->getFile()}' in line: {$ee->getLine()}");
+			throw $ee;
+		}
+		catch (Exception $e)
+		{
+			return new Response("Registro no encontrado ". $e, Response::HTTP_NOT_FOUND);
+			$logger->error("({$e->getCode()}) Message: '{$e->getMessage()}' in file: '{$e->getFile()}' in line: {$e->getLine()}");
+			throw $e;
+		}
 
 
-		//print exec("echo hola")."\n";
-		return new Response('<html><body>Lucky number is :' . $number . '</body>');
 
 	}
 
