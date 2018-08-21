1600c6f3b846e0cb99748f0449b207cd143e19bc
jesus yepes <jesusyepes.1205@gmail.com>
2018-08-22 00:49:38 +0200

    Mejorande la legibilidad del codigo dentro del script .sh

--- csvReader/csvReaderDaemon.sh
+++ csvReader/csvReaderDaemon.sh
@@ -1,33 +1,9 @@
-
-##!/bin/bash/env php
 #!/bin/bash
-echo "hola mundo"
-#while true; do
-
-#sleep 0.05
+echo "Ejecutando comando CsvImport"
 
 today=`date +%d/%m/%Y`
 hour=`date +"%H:%M:%S`
 
-echo "Hoy es el dia $today y  hora actual es: $hour"
+echo "en el dia $today y la hora actual es: $hour"
 
 php /home/maggie/Documentos/Aplicaciones/symfonyRest/restTranscoma/csvReader/bin/console csv:import
-
-#while true; do
-#php ./../bin/console csv:import
-
-#echo "hola"
-#sleep 0.05
-#done
-
-##!/usr/bin/env php
-## app/console
-#<?php
-
-#use AppBundle\Command\CsvImportCommand;
-#use Symfony\Component\Console\Application;
-
-#$application = new Application();
-#$application->add(new CsvImportCommand);
-#$application->run();
-#>
