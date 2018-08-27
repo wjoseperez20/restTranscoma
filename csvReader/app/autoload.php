624b38b3bb1eea9fce46a06a12fe62d95aca1013
jesus yepes <jesusyepes.1205@gmail.com>
2018-08-27 20:17:29 +0200

    Insertando configuration and setup de mongodb dentro de autoload.ph

--- csvReader/app/autoload.php
+++ csvReader/app/autoload.php
@@ -3,9 +3,18 @@
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
+/** Agregando configuracion y setup para mongodb*/
+use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
+
+AnnotationDriver::registerAnnotationClasses();
+
 /** @var ClassLoader $loader */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
 return $loader;
+
+
+
+
