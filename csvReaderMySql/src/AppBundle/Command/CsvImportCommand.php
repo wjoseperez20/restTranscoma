fd7456069cb68c6b64ba251859001003dd68b148
jesus yepes <jesusyepes.1205@gmail.com>
2018-08-30 15:14:23 +0200

    Arreglando detalles de ubicacion de los logs y de la carpeta fabrica

--- csvReaderMySql/src/AppBundle/Command/CsvImportCommand.php
+++ csvReaderMySql/src/AppBundle/Command/CsvImportCommand.php
@@ -11,10 +11,8 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-
 use Monolog\Logger;
-//use MonologHandlerLogglyHandler;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
 
 
 /**
