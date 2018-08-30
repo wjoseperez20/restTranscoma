ba311ffe850911dc1a8d41ed7cb6432d73cb020a
jesus yepes <jesusyepes.1205@gmail.com>
2018-08-30 05:46:17 +0200

    instalando configutacion dentro de appKernel registrando los bundles de inicio

--- csvReaderMySql/app/AppKernel.php
+++ csvReaderMySql/app/AppKernel.php
@@ -17,15 +17,20 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+	        new FOS\RestBundle\FOSRestBundle(),
+	        new JMS\SerializerBundle\JMSSerializerBundle(),
+	        new Nelmio\CorsBundle\NelmioCorsBundle()
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+	        $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
 
             if ('dev' === $this->getEnvironment()) {
-                $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+               //// $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
                 $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
             }
         }
@@ -50,12 +55,12 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(function (ContainerBuilder $container) {
-            $container->setParameter('container.autowiring.strict_mode', true);
-            $container->setParameter('container.dumper.inline_class_loader', true);
-
-            $container->addObjectResource($this);
-        });
+//        $loader->load(function (ContainerBuilder $container) {
+//            $container->setParameter('container.autowiring.strict_mode', true);
+//            $container->setParameter('container.dumper.inline_class_loader', true);
+//
+//            $container->addObjectResource($this);
+//        });
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
 }
