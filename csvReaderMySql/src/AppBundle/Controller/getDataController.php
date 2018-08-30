d8273eefcfaa7c3eb6855b40fad13b753057667e
jesus yepes <jesusyepes.1205@gmail.com>
2018-08-30 06:08:02 +0200

    Creando dos metodos en la cual realiza la consulta en myql todo el documento dua, y otra consulta teniendo como parametro el id, donde estos metodos retornan un json

---
+++ csvReaderMySql/src/AppBundle/Controller/getDataController.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maggie
+ * Date: 17/08/18
+ * Time: 16:31
+ */
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\View\View;
+
+
+class getDataController extends FOSRestController
+{
+
+	/**
+	 * Metodo que retorna todos los registros del documento
+	 * DuaPartidas.csv mediante Doctrine desde mysql
+	 * Ejemplo:  http://localhost:8000/postal
+	 * @Rest\Get("/postal")
+	 */
+	public function getAction()
+	{
+		$rest_result = $this->getDoctrine()->getRepository('AppBundle:Postal')->findAll();
+		if ($rest_result === null)
+		{
+			return new View("No existen registros dentro del documento DataPartidasDua.csv",
+				Response::HTTP_NOT_FOUND);
+		}
+		return $rest_result;
+	}
+
+	/**
+	 * metodo que retorna un registro del documento DataPartidasDua por id.
+	 * ejemplo: http://localhost:8000/postal/38000
+	 * @Rest\Get("/postal/{id}")
+	 * @param $id
+	 * @return View|null|object
+	 */
+	public function idAction($id)
+	{
+		$single_result = $this->getDoctrine()->getRepository('AppBundle:Postal')->find($id);
+		if ($single_result === null)
+		{
+			throw $this->createNotFoundException('Registro no encontrado con el id '.$id);
+			return new View("Registro no encontrado", Response::HTTP_NOT_FOUND);
+		}
+		return $single_result;
+	}
+
+}
