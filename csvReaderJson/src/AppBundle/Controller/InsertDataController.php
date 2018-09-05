56f30932de5033d1380270be89c8514aa78230bc
jesus yepes <jesusyepes.1205@gmail.com>
2018-09-05 21:46:59 +0200

    Agregando funcion peticion post, que falta por probar e implementar y probar

--- csvReaderJson/src/AppBundle/Controller/InsertDataController.php
+++ csvReaderJson/src/AppBundle/Controller/InsertDataController.php
@@ -10,6 +10,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use AppBundle\Document\PostalDua;
 use AppBundle\Factory\LoggerFactory;
@@ -168,7 +169,59 @@ class InsertDataController extends FOSRestController
         }
 
     }// fin validarCadenaVacia
+
+
+    /**
+     *
+     */
+    public function peticion_post(){
+
+
+
+        $url = "https://www.servicioprueba.com/request";
+
+        $conexion = curl_init();
+
+        $envio = "datos que se envian"; // --- Puede ser un xml, un json, etc.
+
+
+
+        curl_setopt($conexion, CURLOPT_URL,$url);
+
+// --- Datos que se van a enviar por POST.
+
+        curl_setopt($conexion, CURLOPT_POSTFIELDS,$envio);
+
+// --- Cabecera incluyendo la longitud de los datos de envio.
+
+        curl_setopt($conexion, CURLOPT_HTTPHEADER,array('Content-Type: application/json', 'Content-Length: '.strlen($envio)));
+
+// --- Petición POST.
+
+        curl_setopt($conexion, CURLOPT_POST, 1);
+
+// --- HTTPGET a false porque no se trata de una petición GET.
+
+        curl_setopt($conexion, CURLOPT_HTTPGET, FALSE);
+
+// -- HEADER a false.
+
+        curl_setopt($conexion, CURLOPT_HEADER, FALSE);
+
+
+
+// --- Respuesta.
+
+        $respuesta=curl_exec($conexion);
+
+
+
+
+curl_close($conexion);
+
 }
+}
+
 
 
 //------------------------------------------------- Estructura Post ----------------------------------------------
