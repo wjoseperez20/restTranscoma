bfa10f01fe608e1b5e1bb01a0219a455d639815b
jesus yepes <jesusyepes.1205@gmail.com>
2018-09-07 16:04:50 +0200

    Quitando la funcion de envio post (peticion_postAction) para colocarlo en la clase postDataController. En lugar de llamar esta funcion dentro de InsertDataController se invoca a la fabrica que contiene dicha instancia

--- csvReaderJson/src/AppBundle/Controller/InsertDataController.php
+++ csvReaderJson/src/AppBundle/Controller/InsertDataController.php
@@ -8,8 +8,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Factory\ControllerFactory;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations as Rest;
@@ -30,7 +30,6 @@ use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class InsertDataController extends FOSRestController
 {
-
 
     /**
      * Constantes para esteblecer parametros de los loggers
@@ -61,6 +60,7 @@ class InsertDataController extends FOSRestController
         $logger = LoggerFactory::getLogger(self::CLASS_NAME);
         $handler = LoggerFactory::getStreamHandler($log_directory);
         $logger->pushHandler($handler);
+        $envio_post=ControllerFactory::getPostDataController();
 
         try
         {
@@ -124,7 +124,7 @@ class InsertDataController extends FOSRestController
                 //$jsonContentD =$serializer->deserialize($jsonContent,'json');
 
                 //Llamando a la funcion peticion_post
-                $this->peticion_postAction($jsonContent);
+                $envio_post->peticion_postAction($jsonContent);
 
             } //fin de foreach
 
@@ -165,7 +165,13 @@ class InsertDataController extends FOSRestController
      */
     public function validarCadenaVacia($valor)
     {
+        /* Carga de variables de entorno desde el archivo.env*/
+        $dotenv = DotenvFactory::getDotEnv();
+
+        /*indicando el archivo .env mediante ruta absoluta*/
+        $dotenv->load('/home/maggie/Documentos/Aplicaciones/symfonyRest/restTranscoma/csvReaderJson/.env');
         $log_directory= getenv('LOG_DIRECTORY');
+
         $logger = LoggerFactory::getLogger(self::CLASS_NAME);
         $handler = LoggerFactory::getStreamHandler($log_directory);
         $logger->pushHandler($handler);
@@ -185,71 +191,6 @@ class InsertDataController extends FOSRestController
 
     }// fin validarCadenaVacia
 
-
-    /**
-     * realiza una peticion post hacia una url especificada, y con el parametro en formato json
-     * @Route("verificar", name="verificar")
-     * @param string $envio
-     * @return View
-     * @throws \Exception
-     */
-    public function peticion_postAction($envio='{"id": 2000,"tracking_number": "PQ48K20476017570107300Z",
-    "conocimiento_aereo": "20180620FDX5245772500908957","reference": "83390767643","bag_label": "LS1002315891"}')
-    {
-        $log_directory= getenv('LOG_DIRECTORY');
-        $logger = LoggerFactory::getLogger(self::CLASS_NAME);
-        $handler = LoggerFactory::getStreamHandler($log_directory);
-        $logger->pushHandler($handler);
-
-        /*url de prueba para peticiones post
-        Esta url permite probar peticiones post donde se visualiza el formato json y los datos de entrada en un cuerpo
-        que muestra su retorno en html. En el campo donde dice json se muestra cada elemento leido del documento en formato json
-        */
-        $url = "http://httpbin.org/post";
-        //$url = "http://localhost:8002/user/";
-
-        try
-        {
-            // --- inicia la conexion inicializando el objeto curl
-            $conexion = curl_init();
-
-            //$envio = "datos que se envian"; // --- Puede ser un xml, un json, etc.
-
-            curl_setopt($conexion, CURLOPT_URL,$url);
-
-            // --- Datos que se van a enviar por POST.
-            curl_setopt($conexion, CURLOPT_POSTFIELDS,$envio);
-
-            // --- Cabecera incluyendo la longitud de los datos de envio.
-            curl_setopt($conexion, CURLOPT_HTTPHEADER,array('Content-Type: application/json', 'Content-Length: '.strlen($envio)));
-
-            // --- Petición POST.
-            curl_setopt($conexion, CURLOPT_POST, 1);
-
-            // --- HTTPGET a false porque no se trata de una petición GET.
-            curl_setopt($conexion, CURLOPT_HTTPGET, FALSE);
-
-            // -- HEADER a false.
-            curl_setopt($conexion, CURLOPT_HEADER, FALSE);
-
-            // --- Respuesta.
-            $respuesta=curl_exec($conexion);
-
-            //$logger->info('la respuesta es '.$respuesta);
-
-            // -- Cerrando conexion
-            curl_close($conexion);
-
-            return new View(' probando metodo '.$respuesta,Response::HTTP_OK);
-        }
-        catch (\Exception $e)
-        {
-            $logger
-                ->error("({$e->getCode()}) Message: '{$e->getMessage()}' in file: '{$e->getFile()}' in line: {$e->getLine()}");
-            throw $e;
-        }
-
-    }// fin de metodo postAction
 
 }// fin de InsertDataController
 
