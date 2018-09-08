5225ecdde6350f6e3f5482331cce69ac3e4250d6
jesus yepes <jesusyepes.1205@gmail.com>
2018-09-08 21:51:18 +0200

    Colocando rutas relativas en el demonio .sh para la ejecucion del csv:import

--- csvReaderJson/csvReaderDaemon.sh
+++ csvReaderJson/csvReaderDaemon.sh
@@ -7,4 +7,4 @@ hour=`date +%H:%M:%S`
 
 echo "en el dia $today y la hora actual es: $hour"
 
-php /home/maggie/Documentos/Aplicaciones/symfonyRest/restTranscoma/csvReaderMongoDB/bin/console csv:import
+php ./../bin/console csv:import
