975764e4b9a92db9f39403f00f77c94157be2270
jesus yepes <jesusyepes.1205@gmail.com>
2018-09-14 19:30:08 +0200

    Refactorizando codigo dentro de CsvImportCommand

--- csvReaderJson/src/AppBundle/Command/CsvImportCommand.php
+++ csvReaderJson/src/AppBundle/Command/CsvImportCommand.php
@@ -4,6 +4,7 @@ namespace AppBundle\Command;
 
 use AppBundle\Command\HandleFile\ReadFileYml;
 use AppBundle\Controller\PostDataController;
+use Doctrine\Bundle\DoctrineCacheBundle\Tests\Functional\FileSystemCacheTest;
 use Doctrine\ORM\EntityManagerInterface;
 
 use League\Csv\Reader;
@@ -80,7 +81,7 @@ class CsvImportCommand extends ContainerAwareCommand
             $this->logger = LoggerFactory::getLogger(self::CLASS_NAME);
             $this->handler = LoggerFactory::getStreamHandler($this->log_directory);
             $this->logger->pushHandler($this->handler);
-           // $this->send_post = ControllerFactory::getPostDataController();
+            $this->send_post = ControllerFactory::getPostDataController();
         }
         catch (\Exception $e)
         {
@@ -121,130 +122,81 @@ class CsvImportCommand extends ContainerAwareCommand
      * @throws \Exception
      */
     protected function execute(InputInterface $input, OutputInterface $output)
-    {
+     {
         try {
             $this->setLogger();
             $this->logger->info('This process was started in ' . CsvImportCommand::class);
-           $send_post= $this->send_post = ControllerFactory::getPostDataController();
             $io = new SymfonyStyle($input, $output);
-
-            $finder = new Finder();
-            $finder->files()->in($this->csv_directory)->name('*.csv')->exclude('csvRead');
-            $fileSystem = new Filesystem();
-
-            if (!($fileSystem->exists($this->csv_directory . 'csvRead'))) {
-                $fileSystem->mkdir($this->csv_directory . 'csvRead');
-            }
-
-            $encoders = array(new JsonEncoder());
-            $normalizers = array(new ObjectNormalizer());
-            $serializer = new Serializer($normalizers, $encoders);
-
-            $getCol = HandleFileFactory::getReadFileYml();
-
-            foreach ($finder as $file) {
-
-                if (file_get_contents($file)){
-                    $this->readDocument($io,$output,$serializer,$getCol,$fileSystem,$file, $send_post);
-                }
-                elseif (file_get_contents($file) !== false)
-                {
-                    $this->logger->info('The file '.$file->getFilename() .' is empty. Removing the empty file');
-                    $fileSystem->remove(($this->csv_directory) . $file->getFilename());
-                }
-            }
+            $this->finderDirectory($io,$output);
         }
-        catch (\Exception $e)
-        {
-            $this->logger->error("({$e->getCode()}) Message: '{$e->getMessage()}' in file: '{$e->getFile()}' in line: {$e->getLine()}");
+        catch (\Exception $e){
+            $this->logger->error("({$e->getCode()}) Message: '{$e->getMessage()}' in file: '{$e->getFile()}' in line: {$e->getLine()} into execute method");
             throw $e;
         }
-        finally
-        {
+        finally{
             $this->logger->info('The process was finally into CsvImportCommand::execute()');
         }
     }
 
     /**
-     * Validates if the value inside the document is empty, write nuul,
-     * otherwise returns its value
-     * @param $value
-     * @return string
+     * this function searches all the files in csv format located inside assets,
+     * then calls the function (readDocument)
+     * @param SymfonyStyle $io
+     * @param OutputInterface $output
      * @throws \Exception
      */
-    public function validateEmptyString($value)
+    public function finderDirectory( SymfonyStyle $io,OutputInterface $output)
     {
-        try
-        {
-            if (trim($value) == '')
-                return 'NULL';
-            else
-                return $value;
+        try {
+            $finder = new Finder();
+            $finder->files()->in($this->csv_directory)->name('*.csv')->exclude('csvRead');
+            $fileSystem = new Filesystem();
+            $this->validateExistsDirectory($fileSystem);
+            /*param1: normalizer / param2: encoder*/
+            $serializer = new Serializer(array(new ObjectNormalizer()), array(new JsonEncoder()));
+            $getCol = HandleFileFactory::getReadFileYml();
+
+            foreach ($finder as $file) {
+                if (file_get_contents($file)) {
+                    $this->readDocument($io, $output, $serializer, $getCol, $fileSystem, $file);
+                } elseif (file_get_contents($file) !== false) { //if empty file, remove it.
+                    $this->logger->info('The file ' . $file->getFilename() . ' is empty. Removing this empty file');
+                    $fileSystem->remove(($this->csv_directory) . $file->getFilename());
+                }
+            }
         }
-        catch (\Exception $e)
-        {
-            $this->logger
-                ->error("({$e->getCode()}) Message: '{$e->getMessage()}' in file: '{$e->getFile()}' in line: {$e->getLine()}");
+        catch (\Exception $e){
+            $this->logger->error("({$e->getCode()}) Message: '{$e->getMessage()}' in file: '{$e->getFile()}' in line: {$e->getLine()} into scrollDirectory");
             throw $e;
         }
     }
 
-
+    /**
+     *
+     * @param SymfonyStyle $io
+     * @param OutputInterface $output
+     * @param Serializer $serializer
+     * @param ReadFileYml $getCol
+     * @param Filesystem $fileSystem
+     * @param \SplFileInfo $file
+     * @throws \Exception
+     */
     public function readDocument(SymfonyStyle $io,OutputInterface $output, Serializer $serializer,
-                                 ReadFileYml $getCol, Filesystem $fileSystem , \SplFileInfo $file, PostDataController $send_post)
+                                 ReadFileYml $getCol, Filesystem $fileSystem , \SplFileInfo $file)
     {
         $io->title('Reading .csv ...');
-        $postalDua = null;
         $this->logger->info('Reading ' . $file->getFilename() .' file');
         $reader = Reader::createFromPath($file);
         $results = $reader->fetchAssoc();
         $io->progressStart(iterator_count($results));
         $start_time = microtime(true); //true is in seconds
         $pos=0;// pos =0 to indicate the first row, -1 to indicate that the document could not be read
+
         foreach ($results as $row) {
             if ((count($row) >= (string)$getCol->getColumn('headers'))) {
-                $duaImport = (new DuaImport())
-                    ->setTrackingNumber($this->validateEmptyString($row[(string)$getCol->getColumn('column1')]))
-                    ->setConocimientoAereo($this->validateEmptyString($row[(string)$getCol->getColumn('column2')]))
-                    ->setReference($this->validateEmptyString($row[(string)$getCol->getColumn('column3')]))
-                    ->setBagLabel($this->validateEmptyString($row[(string)$getCol->getColumn('column4')]))
-                    ->setOrigin($this->validateEmptyString($row[(string)$getCol->getColumn('column5')]))
-                    ->setDestination($this->validateEmptyString($row[(string)$getCol->getColumn('column6')]))
-                    ->setSumaria($this->validateEmptyString($row[(string)$getCol->getColumn('column7')]))
-                    ->setPartida($row[(string)$getCol->getColumn('column8')])
-                    ->setInternalAccountNumber($this->validateEmptyString($row[(string)$getCol->getColumn('column9')]))
-                    ->setShipperName($this->validateEmptyString($row[(string)$getCol->getColumn('column10')]))
-                    ->setShipAdd1($this->validateEmptyString($row[(string)$getCol->getColumn('column11')]))
-                    ->setShipAdd2($this->validateEmptyString($row[(string)$getCol->getColumn('column12')]))
-                    ->setShipAdd3($this->validateEmptyString($row[(string)$getCol->getColumn('column13')]))
-                    ->setShipCity($this->validateEmptyString($row[(string)$getCol->getColumn('column14')]))
-                    ->setShipState($this->validateEmptyString($row[(string)$getCol->getColumn('column15')]))
-                    ->setShipZip($row[(string)$getCol->getColumn('column16')])
-                    ->setShipCountryCode($this->validateEmptyString($row[(string)$getCol->getColumn('column17')]))
-                    ->setNif($this->validateEmptyString($row[(string)$getCol->getColumn('column18')]))
-                    ->setConsignee($this->validateEmptyString($row[(string)$getCol->getColumn('column19')]))
-                    ->setAddress1($this->validateEmptyString($row[(string)$getCol->getColumn('column20')]))
-                    ->setAddress2($this->validateEmptyString($row[(string)$getCol->getColumn('column21')]))
-                    ->setAddress3($this->validateEmptyString($row[(string)$getCol->getColumn('column22')]))
-                    ->setCity($this->validateEmptyString($row[(string)$getCol->getColumn('column23')]))
-                    ->setState($this->validateEmptyString($row[(string)$getCol->getColumn('column24')]))
-                    ->setZip($row[(string)$getCol->getColumn('column25')])
-                    ->setCountryCode($this->validateEmptyString($row[(string)$getCol->getColumn('column26')]))
-                    ->setEmail($this->validateEmptyString($row[(string)$getCol->getColumn('column27')]))
-                    ->setPhone($this->validateEmptyString($row[(string)$getCol->getColumn('column28')]))
-                    ->setPieces($row[(string)$getCol->getColumn('column29')])
-                    ->setTotalWeight($row[(string)$getCol->getColumn('column30')])
-                    ->setWeightUOM($this->validateEmptyString($row[(string)$getCol->getColumn('column31')]))
-                    ->setTotalValue($this->validateEmptyString($row[(string)$getCol->getColumn('column32')]))
-                    ->setCurrency($this->validateEmptyString($row[(string)$getCol->getColumn('column33')]))
-                    ->setIncoterms($this->validateEmptyString($row[(string)$getCol->getColumn('column34')]))
-                    ->setService($this->validateEmptyString($row[(string)$getCol->getColumn('column35')]))
-                    ->setItemDescription($this->validateEmptyString($row[(string)$getCol->getColumn('column36')]))
-                    ->setItemHsCode($row[(string)$getCol->getColumn('column37')])
-                    ->setItemQuantity($row[(string)$getCol->getColumn('column38')])
-                    ->setItemValue($row[(string)$getCol->getColumn('column39')]);
+                $duaImport= $this->settersDuaImport($row, $getCol);
                 $jsonContent = $serializer->serialize($duaImport, 'json');
-                $send_post->requestPostAction($jsonContent);
+                $this->send_post->requestPostAction($jsonContent);
                 $output->writeln(sprintf("\033\143" ));
                 $output->writeln(sprintf('Processing file reading Csv'."\n"));
                 $io->progressAdvance();
@@ -260,15 +212,97 @@ class CsvImportCommand extends ContainerAwareCommand
             $io->progressFinish();
             $io->success('Command Executed with Success!');
             $end_time = microtime(true);
-            $elapsed_time = $end_time - $start_time;
-            $elapsed_time_min = $elapsed_time / 60;
-            $this->logger->info('Success : Reading time : ' . $elapsed_time_min . ' min. into CsvImportCommand::insertAction');
+            $elapsed_time = ($end_time - $start_time)/60;
+            $this->logger->info('Success : Reading time : ' . $elapsed_time . ' min. into CsvImportCommand::insertAction');
         }
         else {
             $output->writeln("\n");
             $io->note('The document ' . $file->getFilename() . ' reading can not be processed. Check if the headers are complete.');
             $this->logger->info('The document ' . $file->getFilename() . ' reading can not be processed. Check if the headers are complete.');
         }
+    }
 
+    /**
+     * @param $row
+     * @param ReadFileYml $getCol
+     * @return mixed
+     * @throws \Exception
+     */
+    public function settersDuaImport($row, ReadFileYml $getCol)
+    {
+        $duaImport = (new DuaImport())
+            ->setTrackingNumber($this->validateEmptyString($row[(string)$getCol->getColumn('column1')]))
+            ->setConocimientoAereo($this->validateEmptyString($row[(string)$getCol->getColumn('column2')]))
+            ->setReference($this->validateEmptyString($row[(string)$getCol->getColumn('column3')]))
+            ->setBagLabel($this->validateEmptyString($row[(string)$getCol->getColumn('column4')]))
+            ->setOrigin($this->validateEmptyString($row[(string)$getCol->getColumn('column5')]))
+            ->setDestination($this->validateEmptyString($row[(string)$getCol->getColumn('column6')]))
+            ->setSumaria($this->validateEmptyString($row[(string)$getCol->getColumn('column7')]))
+            ->setPartida($row[(string)$getCol->getColumn('column8')])
+            ->setInternalAccountNumber($this->validateEmptyString($row[(string)$getCol->getColumn('column9')]))
+            ->setShipperName($this->validateEmptyString($row[(string)$getCol->getColumn('column10')]))
+            ->setShipAdd1($this->validateEmptyString($row[(string)$getCol->getColumn('column11')]))
+            ->setShipAdd2($this->validateEmptyString($row[(string)$getCol->getColumn('column12')]))
+            ->setShipAdd3($this->validateEmptyString($row[(string)$getCol->getColumn('column13')]))
+            ->setShipCity($this->validateEmptyString($row[(string)$getCol->getColumn('column14')]))
+            ->setShipState($this->validateEmptyString($row[(string)$getCol->getColumn('column15')]))
+            ->setShipZip($row[(string)$getCol->getColumn('column16')])
+            ->setShipCountryCode($this->validateEmptyString($row[(string)$getCol->getColumn('column17')]))
+            ->setNif($this->validateEmptyString($row[(string)$getCol->getColumn('column18')]))
+            ->setConsignee($this->validateEmptyString($row[(string)$getCol->getColumn('column19')]))
+            ->setAddress1($this->validateEmptyString($row[(string)$getCol->getColumn('column20')]))
+            ->setAddress2($this->validateEmptyString($row[(string)$getCol->getColumn('column21')]))
+            ->setAddress3($this->validateEmptyString($row[(string)$getCol->getColumn('column22')]))
+            ->setCity($this->validateEmptyString($row[(string)$getCol->getColumn('column23')]))
+            ->setState($this->validateEmptyString($row[(string)$getCol->getColumn('column24')]))
+            ->setZip($row[(string)$getCol->getColumn('column25')])
+            ->setCountryCode($this->validateEmptyString($row[(string)$getCol->getColumn('column26')]))
+            ->setEmail($this->validateEmptyString($row[(string)$getCol->getColumn('column27')]))
+            ->setPhone($this->validateEmptyString($row[(string)$getCol->getColumn('column28')]))
+            ->setPieces($row[(string)$getCol->getColumn('column29')])
+            ->setTotalWeight($row[(string)$getCol->getColumn('column30')])
+            ->setWeightUOM($this->validateEmptyString($row[(string)$getCol->getColumn('column31')]))
+            ->setTotalValue($this->validateEmptyString($row[(string)$getCol->getColumn('column32')]))
+            ->setCurrency($this->validateEmptyString($row[(string)$getCol->getColumn('column33')]))
+            ->setIncoterms($this->validateEmptyString($row[(string)$getCol->getColumn('column34')]))
+            ->setService($this->validateEmptyString($row[(string)$getCol->getColumn('column35')]))
+            ->setItemDescription($this->validateEmptyString($row[(string)$getCol->getColumn('column36')]))
+            ->setItemHsCode($row[(string)$getCol->getColumn('column37')])
+            ->setItemQuantity($row[(string)$getCol->getColumn('column38')])
+            ->setItemValue($row[(string)$getCol->getColumn('column39')]);
+        return $duaImport;
+    }
+
+    /**
+     * Validates if the value inside the document is empty, write null,
+     * otherwise returns its value
+     * @param $value
+     * @return string
+     * @throws \Exception
+     */
+    public function validateEmptyString($value)
+    {
+        try{
+            if (trim($value) == '')
+                return 'NULL';
+            else
+                return $value;
+        }
+        catch (\Exception $e){
+            $this->logger
+                ->error("({$e->getCode()}) Message: '{$e->getMessage()}' in file: '{$e->getFile()}' in line: {$e->getLine()}");
+            throw $e;
+        }
+    }
+
+    /**
+     * Validates if exist the folder directory (csvRead), if it not exists then it creates
+     * @param Filesystem $fileSystem
+     */
+    public function validateExistsDirectory(FileSystem $fileSystem)
+    {
+        if (!($fileSystem->exists($this->csv_directory . 'csvRead'))) {
+            $fileSystem->mkdir($this->csv_directory . 'csvRead');
+        }
     }
 }
